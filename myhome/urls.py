cc5d0aabb3dba3066959e54fcfb686c6bd8c4b80
zionchao <zionchao@qq.com>
2016-03-29 21:29:44 +0800

    修复url

--- myhome/urls.py
+++ myhome/urls.py
@@ -14,17 +14,17 @@ Including another URLconf
     1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 #-*-coding:utf-8-*-
-from django.conf.urls import include, url
+from django.conf.urls import patterns,include, url
 from django.contrib import admin
-
+from article.views import RSSFeed
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'article.views.home',name='home'),
-    url(r'^about/$','article.views.home',name="about"),
-    url(r'^message/$','article.views.home',name="message"),
-    url(r'^archive/$','article.views.home',name="archive"),
-    url(r'^feed/$', 'article.views.home', name = "RSS"),
+    url(r'^about/$','article.views.about',name="about"),
+    url(r'^message/$','article.views.message',name="message"),
+    url(r'^archive/$','article.views.archive',name="archive"),
+    url(r'^feed/$', RSSFeed(), name = "RSS"),
 
     url(r'^article/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<id>\d+)/$', 'article.views.detail', name="detail"),#每篇文章
     url(r'^article/(?P<year>\d{4})/(?P<month>\d{1,2})/$','article.views.archive_month',name="archive_month"),#按月归档
